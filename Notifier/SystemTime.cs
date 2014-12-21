eaa781763f11cf6620b707c79c1ec5399e975290
Adam Furtenbach <adam.furtenbach@activesolution.se>
2014-12-21 11:54:15 +0100

    extraced watchdog to own class

---
+++ Notifier/SystemTime.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Prislistan.Framework
+{
+    public class SystemTime
+    {
+        public static Func<DateTime> Method;
+
+        static SystemTime()
+        {
+            Reset();
+        }
+
+        public static DateTime Now
+        {
+            get { return Method(); }
+        }
+
+        public static DateTime Today
+        {
+            get { return Now.Date; }
+        }
+
+        public static void Reset()
+        {
+            Method = () => DateTime.Now;
+        }
+    }
+}
