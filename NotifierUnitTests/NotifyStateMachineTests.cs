056ec55f95587ffedbaa9b16d013a56e6242d4da
Adam Furtenbach <adam.furtenbach@activesolution.se>
2014-12-21 17:52:52 +0100

    fler tester

--- NotifierUnitTests/NotifyStateMachineTests.cs
+++ NotifierUnitTests/NotifyStateMachineTests.cs
@@ -95,7 +95,7 @@ namespace NotifierUnitTests
         [Test]
         public void Trigger_IsTryPromoteToMaster_ExpectedResult()
         {
-            _machine.LastHeartbeat = SystemTime.Now.Add(-TimeSpan.FromSeconds(NotifyStateMachine.SecondsBetweenHeartbeats));
+            _machine.PromoteToMasterTimer = SystemTime.Now.Add(-TimeSpan.FromSeconds(NotifyStateMachine.SecondsToWaitBeforeAttemptingBecomeMasterAfterHeartbeat));
             _machine.State = NotifyState.TryPromoteToMaster;
 
             _machine.Trigger();
@@ -140,6 +140,57 @@ namespace NotifierUnitTests
             Assert.That(_machine.State, Is.EqualTo(NotifyState.Master));
             _client.Received().Heartbeat();
             _watchdog.Received().SetTimeout(TimeSpan.FromSeconds(NotifyStateMachine.SecondsBetweenHeartbeats));
+        }
+
+        [Test]
+        public void Trigger_IsTryPromoteToMaster_ExpectedResult2()
+        {
+            _machine.PromoteToMasterTimer = SystemTime.Now;
+            _machine.State = NotifyState.TryPromoteToMaster;
+
+            _machine.Trigger();
+
+            Assert.That(_machine.State, Is.EqualTo(NotifyState.TryPromoteToMaster));
+            _client.DidNotReceive().Heartbeat();
+            _watchdog.DidNotReceive().SetTimeout(TimeSpan.FromSeconds(NotifyStateMachine.SecondsBetweenHeartbeats));
+        }
+
+        [Test]
+        public void Trigger_IsMaster_ExpectedResult2()
+        {
+            _machine.LastHeartbeat = SystemTime.Now;
+            _machine.State = NotifyState.Master;
+
+            _machine.Trigger();
+
+            Assert.That(_machine.State, Is.EqualTo(NotifyState.Master));
+            Assert.That(_machine.LastHeartbeat, Is.EqualTo(_now));
+            _watchdog.DidNotReceive().SetTimeout(TimeSpan.FromSeconds(NotifyStateMachine.SecondsBetweenHeartbeats));
+        }
+
+        [Test]
+        public void Trigger_IsSlave_ExpectedResult2()
+        {
+            _machine.LastHeartbeat = SystemTime.Now;
+            _machine.State = NotifyState.Slave;
+
+            _machine.Trigger();
+
+            Assert.That(_machine.State, Is.EqualTo(NotifyState.Slave));
+            _watchdog.DidNotReceive().SetTimeout(TimeSpan.FromSeconds(NotifyStateMachine.SecondToWaitBetweenPreliminaryMasterAndMaster));
+        }
+
+        [Test]
+        public void Trigger_IsPreliminaryMaster_ExpectedResult2()
+        {
+            _machine.LastHeartbeat = SystemTime.Now;
+            _machine.State = NotifyState.PreliminaryMaster;
+
+            _machine.Trigger();
+
+            Assert.That(_machine.State, Is.EqualTo(NotifyState.PreliminaryMaster));
+            _client.DidNotReceive().Heartbeat();
+            _watchdog.DidNotReceive().SetTimeout(TimeSpan.FromSeconds(NotifyStateMachine.SecondsBetweenHeartbeats));
         }
     }
 }
